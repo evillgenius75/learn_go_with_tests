1f3978b14008b4efbfe9b4be06076964ffe0c616
Eddie Villalba <eddiejv@google.com>
2021-07-27 04:34:49 -0500

    added variadic function testing

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,22 @@ func Sum(nums []int) int {
 	}
 	return result
 }
+
+func SumAll(numsToSum ...[]int) (sums []int) {
+	for _, numbers := range numsToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numsToSum ...[]int) (sums []int) {
+	for _, numbers := range numsToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Collection of 5 numbers", func(t *testing.T) {
@@ -11,5 +14,44 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("wanted %d, but got %d from %v", want, got, numbers)
 		}
+	})
+}
+
+func TestSummAll(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("wanted %v but got %v", want, got)
+		}
+	}
+
+	t.Run("variadic sum function with multiple slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		checkSums(t, got, want)
+
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("wanted %v but got %v", want, got)
+		}
+	}
+
+	t.Run("variadic sum function with multiple slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely run if an empty slice is given", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1}, []int{0, 9})
+		want := []int{0, 0, 9}
+
+		checkSums(t, got, want)
 	})
 }
