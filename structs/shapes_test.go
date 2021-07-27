2c9fbb81300625cfa0031313b5a0e6e16cc7df12
Eddie Villalba <eddiejv@google.com>
2021-07-27 09:31:56 -0500

    updated for structs

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -4,8 +4,9 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	t.Run("Perimeter tests:", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
 		want := 40.0
-		if got := Perimeter(10.0, 10.0); got != want {
+		if got := Perimeter(rectangle); got != want {
 			t.Errorf("Perimeter() = %v, want %v", got, want)
 		}
 	})
@@ -13,8 +14,9 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	t.Run("Area tests:", func(t *testing.T) {
-		want := 100.0
-		if got := Area(10.0, 10.0); got != want {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+		if got := Area(rectangle); got != want {
 			t.Errorf("Area() = %v, want %v", got, want)
 		}
 	})
